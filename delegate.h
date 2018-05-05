09922ae015cebefca3bd7345c6aca9e368e46b46
陳俊宇 <f64051114@mail.ncku.edu.tw>
2018-05-05 21:57:36 +0800

    Upload New File

---
+++ delegate.h
@@ -0,0 +1,26 @@
+#ifndef DELEGATE_H
+#define DELEGATE_H
+
+#include <QObject>
+#include <QModelIndex>
+#include <QItemDelegate>
+#include <Qsize>
+#include <QSpinBox>
+
+class Delegate : public QItemDelegate
+{
+    Q_OBJECT
+public:
+    explicit Delegate(QObject *parent = 0);
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+    void setEditorData(QWidget *editor, const QModelIndex &index) const;
+    void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
+    void updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+
+signals:
+
+public slots:
+
+};
+
+#endif // DELEGATE_H
